b8a42278109a45640d76a15ce2c650c76ef74a6e
vlad706 <vlad@athemes.com>
2020-01-03 15:13:24 +0200

    1.58

--- header.php
+++ header.php
@@ -38,7 +38,10 @@
 				<div class="col-md-4 col-sm-8 col-xs-12">
 		        <?php if ( get_theme_mod('site_logo') ) : ?>
 					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo('name'); ?>"><img class="site-logo" src="<?php echo esc_url(get_theme_mod('site_logo')); ?>" alt="<?php bloginfo('name'); ?>" /></a>
-		        <?php else : ?>
+					<?php if ( is_home() && !is_front_page() ) : ?>
+						<h1 class="site-title screen-reader-text"><?php bloginfo( 'name' ); ?></h1>
+					<?php endif; ?>
+				<?php else : ?>
 					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>	        
 		        <?php endif; ?>
