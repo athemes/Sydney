7664ed8450dc36e75b4f9b154a50b9f1de4a5732
vlad706 <vlad@athemes.com>
2021-09-09 12:12:50 +0300

    loop content template file

--- content.php
+++ content.php
@@ -4,40 +4,10 @@
  */
 ?>
 
+<?php do_action( 'sydney_before_loop_entry' ); ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-	<?php if ( has_post_thumbnail() && ( get_theme_mod( 'index_feat_image' ) != 1 ) ) : ?>
-		<div class="entry-thumb">
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('sydney-large-thumb'); ?></a>
-		</div>
-	<?php endif; ?>
-
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="title-post entry-title" ' . sydney_get_schema( "headline" ) . '><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' == get_post_type() && get_theme_mod('hide_meta_index') != 1 ) : ?>
-		<div class="meta-post">
-			<?php sydney_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-post" <?php sydney_do_schema( 'entry_content' ); ?>>
-		<?php if ( (get_theme_mod('full_content_home') == 1 && is_home() ) || (get_theme_mod('full_content_archives') == 1 && is_archive() ) ) : ?>
-			<?php the_content(); ?>
-		<?php else : ?>
-			<?php the_excerpt(); ?>
-		<?php endif; ?>
-
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'sydney' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-post -->
-
-	<footer class="entry-footer">
-		<?php sydney_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+	<?php do_action( 'sydney_loop_post' ); ?>
+	
 </article><!-- #post-## -->
+<?php do_action( 'sydney_after_loop_entry' ); ?>
