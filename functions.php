a7770c0fc68f6a471651a615fdb6a274d50c15a2
vlad706 <vlad@athemes.com>
2022-08-24 10:10:52 +0300

    update normalize, clean bootstrap

--- functions.php
+++ functions.php
@@ -75,7 +75,7 @@ function sydney_setup() {
 
 	// Set up the WordPress core custom background feature.
 	add_theme_support( 'custom-background', apply_filters( 'sydney_custom_background_args', array(
-		'default-color' => 'ffffff',
+		'default-color' => 'f7f7f7',
 		'default-image' => '',
 	) ) );
 
